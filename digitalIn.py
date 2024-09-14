e8e5c45f70327c7b33994d678f281d30ece25ac1
daueusa <daueusa@gmail.com>
2024-09-14 19:03:09 +0900

    in change

--- digitalIn.py
+++ digitalIn.py
@@ -1,7 +1,7 @@
 import gpiozero
 import time
 
-pin = gpiozero.DigitalInputDevice(pin=27, pull_up=True)
+pin = gpiozero.DigitalInputDevice(pin=17, pull_up=True)
 try:
     while True:
         print(pin.value)
