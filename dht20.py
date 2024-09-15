4893adbef4d14c55f6dd3db83eb391d73909215f
daueusa <daueusa@gmail.com>
2024-09-15 23:02:35 +0900

    Update dht20.py

--- dht20.py
+++ dht20.py
@@ -1,8 +1,9 @@
 import time                          # 必要なライブラリーのインポート
 import board
+import busio
 import adafruit_ahtx0
 
-i2c = board.I2C()                    # I2Cバスの準備
+i2c = busio.I2C(board.GP1,board.GP0)                    # I2Cバスの準備
 sensor = adafruit_ahtx0.AHTx0(i2c)   # センサーオブジェクトを作成
 
 while True:
