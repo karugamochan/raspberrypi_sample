69d163823c5678bd10dcc6b6566ad7f973587c48
takayasu ueda <daueusa@gmail.com>
2024-09-26 15:43:48 +0900

    Add files via upload

--- dht20.py
+++ dht20.py
@@ -1,12 +1,23 @@
-import time                          # 必要なライブラリーのインポート
+import time
 import board
 import busio
 import adafruit_ahtx0
 
-i2c = busio.I2C(board.GP1,board.GP0)                    # I2Cバスの準備
-sensor = adafruit_ahtx0.AHTx0(i2c)   # センサーオブジェクトを作成
-
+# I2C
+i2c = busio.I2C(board.SCL,board.SDA)
+#dht20 = adafruit_dht.DHT20(board.I2C())
+sensor = adafruit_ahtx0.AHTx0(i2c)
 while True:
-    print("\nTemperature: %0.1f C" % sensor.temperature)     #温度を表示
-    print("Humidity: %0.1f %%" % sensor.relative_humidity)   #湿度を表示　
-    time.sleep(2)
+    try:
+        # 
+        temperature = sensor.temperature
+        humidity = sensor.relative_humidity
+
+        print(f"Temperature: {temperature:.2f} C")
+        print(f"Humidity: {humidity:.2f} %")
+
+    except RuntimeError as error:
+        # 
+        print(f"Error reading data: {error}")
+
+    time.sleep(2)  # 2
